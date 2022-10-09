d8511e58302bb48bcac3c803f94993034dbccda0
Cyan-Xmw <843348394@qq.com>
2022-10-09 17:21:20 +0800

    perf(User-Management): 完善用户管理表单和新增接口操作

--- Xmw_server/app/controller/user.ts
+++ Xmw_server/app/controller/user.ts
@@ -4,18 +4,9 @@
  * @Author: Cyan
  * @Date: 2022-10-09 15:27:18
  * @LastEditors: Cyan
- * @LastEditTime: 2022-10-09 15:48:51
+ * @LastEditTime: 2022-10-09 17:20:36
  */
 import BaseController from './base'
-
-// let OSS = require('ali-oss');
-// // 阿里云对象存储配置
-// let client = new OSS({
-//     region: 'oss-cn-shenzhen',
-//     accessKeyId: 'LTAI4G5oHtJSRFXjq7ie45xh',
-//     accessKeySecret: 'bAqE12OuQ9uHwxkdTltOWRogJUXfmG',
-//     bucket: 'react-umi-xmw',
-// });
 
 /**
  * @description: BaseController 里面的方法不解构执行，目前原因暂不明￣□￣｜｜
@@ -32,8 +23,6 @@ export default class User extends BaseController{
     public async uploadUserAvatar(){
         const { ctx } = this;
         try {
-            // let stream = await ctx.getFileStream();
-            // let result = await client.putStream(stream.filename, stream);
             this.resResult(1, {});
         } catch (error) {
             ctx.logger.info('uploadUserAvatar方法报错：' + error)
