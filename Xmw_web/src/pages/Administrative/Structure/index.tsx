f147d54b6bd81894eb6557141ef57793f53174cb
Cyan-Xmw <843348394@qq.com>
2023-01-11 10:17:31 +0800

    perf(Structure): 优化代码逻辑

--- Xmw_web/src/pages/Administrative/Structure/index.tsx
+++ Xmw_web/src/pages/Administrative/Structure/index.tsx
@@ -4,35 +4,29 @@
  * @Author: Cyan
  * @Date: 2022-09-24 11:16:36
  * @LastEditors: Cyan
- * @LastEditTime: 2023-01-03 13:52:45
+ * @LastEditTime: 2023-01-11 10:16:33
  */
 import { useRequest } from 'ahooks'
 import { Card } from 'antd'
 import type { FC } from 'react';
-import { useIntl } from '@umijs/max'
 import { PageContainer } from '@ant-design/pro-components' // antd 高级组件
 import { getOrganizationList } from '@/services/administrative/organization' // 组织管理接口
 import { formatResult } from '@/utils'
-import { OrganizationGraph } from '@ant-design/charts';
+import { OrganizationGraph, NodeData } from '@ant-design/charts';
 
 const Structure: FC = () => {
-	const { formatMessage } = useIntl();
-
 	/**
 	 * @description: 递归遍历树结构
 	 * @param {*} tree
-	 * @param {*} idField
-	 * @param {*} valueField
-	 * @return {*}
 	 * @author: Cyan
 	 */
-	function loopTree(tree) {
+	function loopTree<T>(tree: (NodeData<{ name?: string }> & T & { [key: string]: string })[], idField: string, nameField: string) {
 		tree.forEach((node) => {
-			node.id = node.org_id
+			node.id = node[idField]
 			node.value = {}
-			node.value.name = node.org_name
+			node.value.name = node[nameField]
 			if (node.children) {
-				loopTree(node.children)
+				loopTree(node.children, idField, nameField)
 			}
 		})
 	}
@@ -45,7 +39,7 @@ const Structure: FC = () => {
 	const { data: orgList, loading } = useRequest<API.ORGANIZATION[], unknown[]>(
 		async () => {
 			const treeData = formatResult(await getOrganizationList())
-			loopTree(treeData)
+			loopTree<API.ORGANIZATION>(treeData, 'org_id', 'org_name')
 			return treeData
 		});
 
@@ -57,7 +51,7 @@ const Structure: FC = () => {
 		children: orgList
 	}
 	return (
-		<PageContainer title={formatMessage({ id: 'pages.administrative.structure' })}>
+		<PageContainer header={{ title: null }}>
 			<Card loading={loading}>
 				<OrganizationGraph data={data} behaviors={['drag-canvas', 'zoom-canvas', 'drag-node']} />
 			</Card>
