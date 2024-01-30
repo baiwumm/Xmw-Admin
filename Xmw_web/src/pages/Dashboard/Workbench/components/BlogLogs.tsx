2c5b24b3bce9467dfbb216e3ea065fccc1d257c6
baiwumm <baiwumm@foxmail.com>
2024-01-30 09:51:55 +0800

    pref: 更新首页文章

--- Xmw_web/src/pages/Dashboard/Workbench/components/BlogLogs.tsx
+++ Xmw_web/src/pages/Dashboard/Workbench/components/BlogLogs.tsx
@@ -3,8 +3,8 @@
  * @Version: 2.0
  * @Author: 白雾茫茫丶
  * @Date: 2023-10-11 10:02:20
- * @LastEditors: 白雾茫茫丶<baiwumm@foxmail.com>
- * @LastEditTime: 2023-12-01 09:15:46
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2024-01-29 09:29:15
  */
 import { useIntl } from '@umijs/max'
 import { Avatar, Card, List, Tooltip } from 'antd'
@@ -34,27 +34,27 @@ const BlogLogs: FC<BlogLogsProps> = ({ renderSecondary }) => {
     {
       title: '基于 Vue3 + TypeScript + Vite + Egg.js 开发的后台应用',
       content: 'Vue3 Admin 基于 Vben Admin 二次开发，它使用了全新的技术栈：Vue3、Vite、TypeScript等，提供了完善的前后端权限管理方案，丰富的主题配置及黑暗主题适配，对日常使用频率较高的组件二次封装,满足基础工作需求，希望本项目可以帮助到您。',
-      link: 'https://baiwumm.com/post/ee4e2857.html',
+      link: 'https://baiwumm.com/p/cy55lsqi',
     },
     {
       title: '基于 Vue2.0 + Egg.js 的后台应用',
       content: 'Vue2 Admin 是一个后台管理系统解决方案，采用前后端分离技术开发。它使用了最新的技术栈，提供了丰富的功能组件，希望本项目可以帮助到您。',
-      link: 'https://baiwumm.com/post/5b5885ee.html',
+      link: 'https://baiwumm.com/p/umcaj6q8',
     },
     {
       title: 'JS 中的二进制散列值和权限设计',
       content: '这篇文章介绍了在JavaScript中使用二进制散列值和位运算符来实现权限设计。作者讨论了权限控制的场景和常见的权限类型，并介绍了JavaScript中提供的进制表示方法和位运算符的应用。作者还提到了位运算符在传统权限系统中解决权限关联和组合的优势，并给出了使用按位或、按位与和按位非来添加、校验和剔除权限的示例。文章提到了位运算符方案的前提条件。',
-      link: 'https://baiwumm.com/post/32205ec4.html',
+      link: 'https://baiwumm.com/p/ts9wfscw',
     },
     {
-      title: '为博客添加 Algolia 全局搜索',
-      content: 'Algolia 是一个数据库实时搜索服务，能够提供毫秒级的数据库搜索服务，并且其服务能以 API 的形式方便地布局到网页、客户端、APP 等多种场景。',
-      link: 'https://baiwumm.com/post/3871aa5b.html',
+      title: '箭头函数的意义和函数的二义性',
+      content: '这篇文章介绍了箭头函数的特点以及与普通函数的区别。它指出了箭头函数没有this、arguments对象和prototype原型的特点，因此不能作为构造函数使用。文章还解释了函数的二义性问题，即函数可以有多种调用方式，这在JS设计上存在缺陷。最后，文章指出箭头函数不再依赖面向对象的概念，因此没有this和原型的概念。',
+      link: 'https://baiwumm.com/p/gar7jcvz',
     },
     {
-      title: '在 vite 中使用 glob 实现约定式路由',
-      content: '约定式路由也叫文件路由，就是不需要手写配置，文件系统即路由，通过目录和文件及其命名分析出路由配置。使用过 React 的同学应该都接触过 Umi 约定式路由 。',
-      link: 'https://baiwumm.com/post/50b97751.html',
+      title: '对象解构与迭代器的猫腻？',
+      content: '这篇文章介绍了变量的解构赋值技巧在前端开发中的常用性，包括对象解构和数组解构。文章中给出了一些代码示例，解释了可迭代对象的概念以及如何使用迭代协议来实现数组解构。此外，文章还介绍了生成器的概念并提供了代码示例。作者建议对这些概念不熟悉的读者可以查阅ES6的文档来更好地理解。',
+      link: 'https://baiwumm.com/p/ek5luclv',
     },
   ]
   return (
