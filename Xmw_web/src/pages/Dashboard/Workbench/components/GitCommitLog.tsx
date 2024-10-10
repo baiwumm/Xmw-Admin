6812a93a4ef2c4c7a293280fa95249448ecd40bf
baiwumm <baiwumm@foxmail.com>
2024-10-10 11:27:55 +0800

    feat(work-bench): 修改首页 Git 更新日志显示条数

--- Xmw_web/src/pages/Dashboard/Workbench/components/GitCommitLog.tsx
+++ Xmw_web/src/pages/Dashboard/Workbench/components/GitCommitLog.tsx
@@ -3,8 +3,8 @@
  * @Version: 2.0
  * @Author: 白雾茫茫丶
  * @Date: 2023-10-11 09:54:01
- * @LastEditors: 白雾茫茫丶
- * @LastEditTime: 2023-10-11 10:00:17
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2024-10-10 11:27:01
  */
 import { useIntl } from '@umijs/max'
 import { useRequest } from 'ahooks'
@@ -29,7 +29,7 @@ const GitCommitLog: FC = () => {
  */
   const { data: commitList, loading: commitLoading } = useRequest(
     async () => {
-      const response = await fetch('https://api.github.com/repos/baiwumm/Xmw-Admin/commits?page=1&per_page=5')
+      const response = await fetch('https://api.github.com/repos/baiwumm/Xmw-Admin/commits?page=1&per_page=10')
       if (isSuccess(response.status)) {
         const result = await response.json()
         return result
