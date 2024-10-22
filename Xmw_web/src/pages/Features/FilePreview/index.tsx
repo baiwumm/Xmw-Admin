610a84a2f63a8ed91dd243e6a0c112aaa01b050b
baiwumm <baiwumm@foxmail.com>
2024-10-22 13:57:06 +0800

    feat(FilePreview): 完善国际化配置

--- Xmw_web/src/pages/Features/FilePreview/index.tsx
+++ Xmw_web/src/pages/Features/FilePreview/index.tsx
@@ -2,35 +2,44 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-10-21 14:42:25
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-10-22 09:37:02
+ * @LastEditTime: 2024-10-22 13:50:49
  * @Description: 文件预览
  */
 import { PageContainer } from '@ant-design/pro-components'
+import { useIntl } from '@umijs/max';
 import { Card, Tabs, type TabsProps } from 'antd';
 import { FC } from 'react';
+
+import { formatPerfix } from '@/utils';
+import { ROUTES } from '@/utils/enums'
 
 import DocxPreview from './components/DocxPreview';
 import ExcelPreview from './components/ExcelPreview';
 import PdfPreview from './components/PdfPreview'
 const FilePreview: FC = () => {
+  const { formatMessage } = useIntl(); // 国际化工具
+
+  // 国际化
+  const renderMessage = (field: string) => formatMessage({ id: formatPerfix(ROUTES.FILEPREVIEW, field) });
+
   const items: TabsProps['items'] = [
     {
       key: '1',
-      label: 'Docx 文件',
+      label: renderMessage('docx'),
       children: (
         <DocxPreview />
       ),
     },
     {
       key: '2',
-      label: 'Excel 文件',
+      label: renderMessage('excel'),
       children: (
         <ExcelPreview />
       ),
     },
     {
       key: '3',
-      label: 'Pdf 文件',
+      label: renderMessage('pdf'),
       children: (
         <PdfPreview />
       ),
